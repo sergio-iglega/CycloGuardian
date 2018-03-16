89501fe613c97f1cc5373aea36f26ea8ac18225b
Sergio <sergio_25_5_95@hotmail.com>
2018-03-16 10:49:59 +0100

    Imagenes en ImageView estático

---
+++ app/src/main/java/com/example/sergi/conexioncamara/Messages/OutcomingCameraMessageTakePhoto.java
@@ -0,0 +1,8 @@
+package com.example.sergi.conexioncamara.Messages;
+
+/**
+ * Created by sergi on 14/03/2018.
+ */
+
+public class OutcomingCameraMessageTakePhoto extends OutcomingCameraMessage {
+}
